40573aa30164e98193d2974d6aaf2fb02befdbb4
Manelima <emanuellima4321@gmail.com>
2024-07-04 01:32:52 -0300

    90% pronto

--- ProjetoWeb_3Ciclo/login.php
+++ ProjetoWeb_3Ciclo/login.php
@@ -94,7 +94,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </html>';
             }
         } else {
-            // Email incorreto
             echo '<!DOCTYPE html>
             <html lang="pt-br">
                 <head>
@@ -158,12 +157,133 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </html>';
         }
 
-    } catch (PDOException $e) {
-        echo "Erro: " . $e->getMessage();
+    } catch (PDOException $e) {  
+      
+      echo '<!DOCTYPE html>
+      <html lang="pt-br">
+          <head>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <link rel="stylesheet" href="css/bootstrap.min.css">
+              <link rel="stylesheet" href="css/style.css">
+              <link rel="stylesheet" href="css/login.css">
+              <title>Aula Bootstrap de David</title>
+          </head>
+          <body class="text-center">
+              <form method="post" class="form-signin" action="login.php">
+                <img class="mb-4" src="imgs/logo.png" alt="" width="72" height="72">
+                <h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
+                <label for="inputEmail" class="sr-only">Endereço de email</label>
+                <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Seu email" required autofocus>
+                <label for="inputPassword" class="sr-only">Senha</label>
+                <input type="password" id="inputPassword" class="form-control" name="senha" placeholder="Senha" required>
+                <div class="checkbox mb-3">
+                  <label>
+                    <input type="checkbox" value="remember-me"> Lembrar de mim
+                  </label>
+                </div>
+                <button class="btn btn-lg btn-savage btn-block" type="submit">Login</button>
+                <p class="mt-5 mb-3 text-muted">&copy; Los Bananas Maduros - 2023</p>
+              </form>
+
+              <!--modal-->
+      
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          Launch demo modal
+        </button>
+        
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="TituloModalCentralizado">Erro de Conexão</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+             Erro: ' . $e->getMessage() . '
+              </div>
+              <div class="modal-footer">
+                <a href="login.html" type="button" class="btn btn-primary">Retornar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+              <script src="js/jquery-3.3.1.slim.min.js"></script>
+              <script src="js/popper.min.js"></script>
+              <script src="js/bootstrap.min.js"></script>
+              <script src="js/script.js"></script>
+              <script>
+                  $("#modal").modal("show")
+              </script>
+          </body>
+      </html>';
     }
 
 } else {
-    echo "Método de requisição inválido.";
+  echo '<!DOCTYPE html>
+  <html lang="pt-br">
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="css/bootstrap.min.css">
+          <link rel="stylesheet" href="css/style.css">
+          <link rel="stylesheet" href="css/login.css">
+          <title>Aula Bootstrap de David</title>
+      </head>
+      <body class="text-center">
+          <form method="post" class="form-signin" action="login.php">
+            <img class="mb-4" src="imgs/logo.png" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
+            <label for="inputEmail" class="sr-only">Endereço de email</label>
+            <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Seu email" required autofocus>
+            <label for="inputPassword" class="sr-only">Senha</label>
+            <input type="password" id="inputPassword" class="form-control" name="senha" placeholder="Senha" required>
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me"> Lembrar de mim
+              </label>
+            </div>
+            <button class="btn btn-lg btn-savage btn-block" type="submit">Login</button>
+            <p class="mt-5 mb-3 text-muted">&copy; Los Bananas Maduros - 2023</p>
+          </form>
+
+          <!--modal-->
+  
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+      Launch demo modal
+    </button>
+    
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="TituloModalCentralizado">Erro de Conexão</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+         Você não possue permissão para acessar o site
+          </div>
+          <div class="modal-footer">
+            <a href="login.html" type="button" class="btn btn-primary">Retornar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+          <script src="js/jquery-3.3.1.slim.min.js"></script>
+          <script src="js/popper.min.js"></script>
+          <script src="js/bootstrap.min.js"></script>
+          <script src="js/script.js"></script>
+          <script>
+              $("#modal").modal("show")
+          </script>
+      </body>
+  </html>';
 }
 
 ?>
